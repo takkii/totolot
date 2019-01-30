50da7fcc5be12ddd226225c686b2ae18837a275e
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-01-31 06:09:52 +0900

    Update.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -7,7 +7,7 @@ from os.path import expanduser
 # ------------------------------- KEYWORD -------------------------------------------------------------------------
 
 home = expanduser("~")
-file = open(os.path.expanduser("~/.vim/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_complete_totolot"))
+file = open(os.path.expanduser("~/.vim/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
 data = file.readlines()
 file.close()
 
