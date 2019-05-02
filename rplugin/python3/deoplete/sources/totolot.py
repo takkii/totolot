24befe87949614e334d47670b82cc462cbb5957d
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-05-02 22:00:28 +0900

    Update.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -43,7 +43,6 @@ class Source(Base):
     def gather_candidates(self, context):
         try:
             dic = data_ruby + data_test + data_report
-            dic.sort(key=lambda dic: dic[0])
             return dic
         except Exception:
             traceback.print_exc()
