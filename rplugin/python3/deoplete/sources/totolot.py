e14cbf99ecba7e2a25ebcffdfb991cd762f25c03
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-10-27 13:59:58 +0900

    Revert.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import re
 import traceback
 from deoplete.source.base import Base
@@ -25,14 +25,14 @@ class Source(Base):
         self.input_pattern = '|'.join(rubymatch + regexmatch)
         self.rank = 500
 
-        def get_complete_position(self, context):
-            m = re.search('[a-zA-Z0-9_?!]*$', context['input'])
-            return m.start() if m else -1
+    def get_complete_position(self, context):
+        m = re.search('[a-zA-Z0-9_?!]*$', context['input'])
+        return m.start() if m else -1
 
-            def gather_candidates(self, context):
-                try:
-                    dic = data_ruby
-                    dic.sort(key=lambda dic: dic[0])
-                    return dic
-                except Exception:
-                    traceback.print_exc()
+    def gather_candidates(self, context):
+        try:
+            dic = data_ruby
+            dic.sort(key=lambda dic: dic[0])
+            return dic
+        except Exception:
+            traceback.print_exc()
