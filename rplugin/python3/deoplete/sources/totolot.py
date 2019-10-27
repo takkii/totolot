258326d6dc6e0c30dce27a1ab1a61c1cebb2a20e
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-10-27 14:05:56 +0900

    Update.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -7,10 +7,22 @@ from os.path import expanduser
 # ------------------------------- KEYWORD -------------------------------------------------------------------------
 
 home = expanduser("~")
-ruby = open(os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
-index_ruby = ruby.readlines()
-data_ruby = list(map(lambda s:s.rstrip(),index_ruby))
-ruby.close()
+
+d1 = os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/")
+d2 = os.path.expanduser("~/.config/nvim/repos/github.com/takkii/ruby-dictionary3/")
+
+if os.path.exists(d1):
+    ruby = open(os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
+elif os.path.exists(d2):
+    ruby = open(os.path.expanduser("~/.config/nvim/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete""))
+else:
+    print('どれにも該当しません、ruby-dictionary3を入れてください。')
+
+    index_ruby = ruby.readlines()
+    data_ruby = list(map(lambda s:s.rstrip(),index_ruby))
+    ruby.close()
+
+
 
 # ------------------------------- KEYWORD -------------------------------------------------------------------------
 
@@ -25,14 +37,14 @@ class Source(Base):
         self.input_pattern = '|'.join(rubymatch + regexmatch)
         self.rank = 500
 
-    def get_complete_position(self, context):
-        m = re.search('[a-zA-Z0-9_?!]*$', context['input'])
-        return m.start() if m else -1
+        def get_complete_position(self, context):
+            m = re.search('[a-zA-Z0-9_?!]*$', context['input'])
+            return m.start() if m else -1
 
-    def gather_candidates(self, context):
-        try:
-            dic = data_ruby
-            dic.sort(key=lambda dic: dic[0])
-            return dic
-        except Exception:
-            traceback.print_exc()
+            def gather_candidates(self, context):
+                try:
+                    dic = data_ruby
+                    dic.sort(key=lambda dic: dic[0])
+                    return dic
+                except Exception:
+                    traceback.print_exc()
