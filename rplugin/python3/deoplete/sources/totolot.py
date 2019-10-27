4011d2fa37af46711a97644e772b813f639855c8
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-10-27 13:48:31 +0900

    Update.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -10,11 +10,17 @@ home = expanduser("~")
 
 d1 = os.path.expanduser("~/.config/nvim/repos/github.com/takkii/ruby-dictionary3/")
 d2 = os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/")
+d2 = os.path.expanduser("~/.vim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/")
+d2 = os.path.expanduser("~/.vim/repos/github.com/takkii/ruby-dictionary3/")
 
 if os.path.exists(d1):
     ruby = open(os.path.expanduser("~/.config/nvim/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
 elif os.path.exists(d2):
     ruby = open(os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
+elif os.path.exists(d3):
+    ruby = open(os.path.expanduser("~/.vim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
+elif os.path.exists(d4):
+    ruby = open(os.path.expanduser("~/.vim/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
 else:
     print('どれにも該当しません、ruby-dictionary3を入れてください。')
 
