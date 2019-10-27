a08fecc992077419d7419479da1579f7822c8901
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-10-27 13:30:11 +0900

    Update.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -13,10 +13,8 @@ d2 = os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby
 
 if os.path.exists(d1):
     ruby = open(os.path.expanduser("~/.config/nvim/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
-elif os.path.exists(d2):
-    ruby = open(os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
 else:
-    print('どれにも該当しません、ruby-dictionary3を入れてください。')
+    ruby = open(os.path.expanduser("~/.config/nvim/.cache/dein/repos/github.com/takkii/ruby-dictionary3/autoload/source/ruby_method_deoplete"))
 
     index_ruby = ruby.readlines()
     data_ruby = list(map(lambda s:s.rstrip(),index_ruby))
