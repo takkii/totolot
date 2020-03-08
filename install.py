28753f9c969288fdd06cc575b752a8e4449dd0f0
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-08 15:12:49 +0900

    Update, install.py

--- install.py
+++ install.py
@@ -1,21 +1,8 @@
-from pip._internal import main as _main
-import importlib
+import pip, site, importlib
 
-def _import(name, module, ver=None):
-    try:
-        globals()[name] = importlib.import_module(module)
-    except ImportError:
-        try:
-            if ver is None:
-                _main(['install', module])
-            else:
-                _main(['install', '{}=={}'.format(module, ver)])
-            globals()[name] = importlib.import_module(module)
-        except:
-            print("can't import: {}".format(module))
+pip.main(['uninstall', 'msgpack-python'])
+#pip.main(['install','msgpack'])
+pip.main(['install','-U','msgpack'])
+pip.main(['install','numpy'])
 
-_import('msg','msgpack', '1.0.0')
-_import('nump','numpy', '1.18.1')
-
-print(msg)
-print(nump)
+importlib.reload(site)
