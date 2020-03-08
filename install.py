6296a602dc9893c8387b24e0cac2498276252162
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-08 15:18:26 +0900

    Update, install.py

--- install.py
+++ install.py
@@ -4,5 +4,6 @@ pip.main(['uninstall', 'msgpack-python'])
 #pip.main(['install','msgpack'])
 pip.main(['install','-U','msgpack'])
 pip.main(['install','numpy'])
+pip.main(['install','pynvim'])
 
 importlib.reload(site)
