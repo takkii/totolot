dc58f7416e2ddaed5082b4f353d3425eff543cb2
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-09 17:27:09 +0900

    Update, GUI design.

--- analyze/corona_ja_result.py
+++ analyze/corona_ja_result.py
@@ -1,10 +1,7 @@
-import tkinter as tk, re
+import re
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
-root = tk.Tk()
-root.title("Coronavirus Tracker")
-root.geometry("800x600")
 session = HTMLSession()
 response = session.get('https://corona-stats.online/Japan')
 soup = BeautifulSoup(response.text,"lxml")
@@ -12,6 +9,3 @@ soup = BeautifulSoup(response.text,"lxml")
 pat = re.compile(r"<[^>]*?>")
 corona_ja_result = (pat.sub("", soup.text))
 print(corona_ja_result)
-tk.Label(root,text=str(corona_ja_result)).pack(fill=tk.X)
-root.mainloop()
-
---
+++ analyze/corona_ja_result_gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Coronavirus Tracker")
+root.geometry("1063x464")
+corosuke = tk.PhotoImage(file="../images/corona_image.png")
+canvas = tk.Canvas(bg="black", width=1063, height=464)
+canvas.place(x=0, y=0)
+canvas.create_image(0, 0, image=corosuke, anchor=tk.NW)
+root.mainloop()
