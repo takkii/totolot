7c69db2537c5c0532a813f11c309359b33934c43
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-10 07:50:47 +0900

    Update, UNIX.

---
+++ analyze/unix_install.py
@@ -0,0 +1,50 @@
+import traceback
+import subprocess
+import importlib, site
+
+class InstallerClass:
+    sci = ['python3', '-m', 'pip', 'install', 'scikit-learn']
+    nump = ['python3', '-m', 'pip', 'install', 'numpy']
+    pan = ['python3', '-m', 'pip', 'install', 'pandas']
+    req = ['python3', '-m', 'pip', 'install', 'requests-html']
+    bs4 = ['python3', '-m', 'pip', 'install', 'beautifulsoup4']
+
+    def sci_method(self):
+        try:
+            ret_sci = subprocess.run(self.sci, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_sci)
+        except Exception:
+            traceback.print_exc()
+    def nump_method(self):
+        try:
+            ret_nump = subprocess.run(self.nump, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_nump)
+        except Exception:
+            traceback.print_exc()
+    def pan_method(self):
+        try:
+            ret_pan = subprocess.run(self.pan, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_pan)
+        except Exception:
+            traceback.print_exc()
+    def req_method(self):
+        try:
+            ret_req = subprocess.run(self.req, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_req)
+        except Exception:
+            traceback.print_exc()
+    def bs4_method(self):
+        try:
+            ret_bs4 = subprocess.run(self.bs4, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_bs4)
+        except Exception:
+            traceback.print_exc()
+
+InstClass = InstallerClass()
+InstClass.sci_method()
+InstClass.nump_method()
+InstClass.pan_method()
+InstClass.req_method()
+InstClass.bs4_method()
+
+importlib.reload(site)
