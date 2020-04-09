5931754cfa2cb756681194160e38d9586598b82a
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-09 16:59:44 +0900

    Update, use Tcl / Tk.

--- analyze/corona_japan_result.py
+++ analyze/corona_japan_result.py
@@ -1,11 +1,17 @@
-import re
+import tkinter as tk, re
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
+root = tk.Tk()
+root.title("Coronavirus Tracker")
+root.geometry("800x600")
 session = HTMLSession()
 response = session.get('https://corona-stats.online/Japan')
 soup = BeautifulSoup(response.text,"lxml")
 [s.decompose() for s in soup('style')]
 pat = re.compile(r"<[^>]*?>")
 corona_ja_result = (pat.sub("", soup.text))
-print(corona_ja_result)
+#print(corona_ja_result)
+tk.Label(root,text=str(corona_ja_result)).pack(fill=tk.X)
+root.mainloop()
+
