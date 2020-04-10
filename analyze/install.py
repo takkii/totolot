7187b90f942c37c63dc6576553acc6ccb0de3237
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-10 16:10:05 +0900

    Update, installer.

--- analyze/install.py
+++ analyze/install.py
@@ -1,13 +1,49 @@
-import traceback
-import subprocess
-import importlib, site
+import traceback, subprocess ,sys
+import importlib, site, platform
 
 class InstallerClass:
-    sci = ['python', '-m', 'pip', 'install', 'scikit-learn']
-    nump = ['python', '-m', 'pip', 'install', 'numpy']
-    pan = ['python', '-m', 'pip', 'install', 'pandas']
-    req = ['python', '-m', 'pip', 'install', 'requests-html']
-    bs4 = ['python', '-m', 'pip', 'install', 'beautifulsoup4']
+    sci_win = ['python', '-m', 'pip', 'install', 'scikit-learn']
+    nump_win = ['python', '-m', 'pip', 'install', 'numpy']
+    pan_win = ['python', '-m', 'pip', 'install', 'pandas']
+    req_win = ['python', '-m', 'pip', 'install', 'requests-html']
+    bs4_win = ['python', '-m', 'pip', 'install', 'beautifulsoup4']
+
+    sci = ['python3', '-m', 'pip', 'install', 'scikit-learn']
+    nump = ['python3', '-m', 'pip', 'install', 'numpy']
+    pan = ['python3', '-m', 'pip', 'install', 'pandas']
+    req = ['python3', '-m', 'pip', 'install', 'requests-html']
+    bs4 = ['python3', '-m', 'pip', 'install', 'beautifulsoup4']
+
+    def sci_win_method(self):
+        try:
+            ret_sci_win = subprocess.run(self.sci_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_sci_win)
+        except Exception:
+            traceback.print_exc()
+    def nump_win_method(self):
+        try:
+            ret_nump_win = subprocess.run(self.nump_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_nump_win)
+        except Exception:
+            traceback.print_exc()
+    def pan_win_method(self):
+        try:
+            ret_pan_win = subprocess.run(self.pan_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_pan_win)
+        except Exception:
+            traceback.print_exc()
+    def req_win_method(self):
+        try:
+            ret_req_win = subprocess.run(self.req_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_req_win)
+        except Exception:
+            traceback.print_exc()
+    def bs4_win_method(self):
+        try:
+            ret_bs4_win = subprocess.run(self.bs4_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_bs4_win)
+        except Exception:
+            traceback.print_exc()
 
     def sci_method(self):
         try:
@@ -40,11 +76,40 @@ class InstallerClass:
         except Exception:
             traceback.print_exc()
 
-InstClass = InstallerClass()
-InstClass.sci_method()
-InstClass.nump_method()
-InstClass.pan_method()
-InstClass.req_method()
-InstClass.bs4_method()
+if sys.version_info[0] == 2:
+  print("This installer is Python2 not supported.")
+
+elif sys.version_info[0] == 3:
+    pf = platform.system()
+    if pf == 'Windows':
+       InstClass = InstallerClass()
+       InstClass.sci_method()
+       InstClass.nump_method()
+       InstClass.pan_method()
+       InstClass.req_method()
+       InstClass.bs4_method()
+
+    elif pf == 'Darwin':
+       InstClass = InstallerClass()
+       InstClass.sci_method()
+       InstClass.nump_method()
+       InstClass.pan_method()
+       InstClass.req_method()
+       InstClass.bs4_method()
+
+    elif pf == 'Linux':
+       InstClass = InstallerClass()
+       InstClass.sci_method()
+       InstClass.nump_method()
+       InstClass.pan_method()
+       InstClass.req_method()
+       InstClass.bs4_method()
+
+    else:
+       print("Windows and MacOS and Linux other OS not supported.")
+
+
+else:
+  print("Python2 and Python3 Version is No match.")
 
 importlib.reload(site)
--- install.py
+++ install.py
@@ -1,14 +1,50 @@
 import sys, traceback
-import subprocess
+import subprocess, platform
 import importlib, site
 
 class InstallerClass:
-    msgpy = ['python', '-m', 'pip', 'uninstall', 'msgpack-python']
-    #msg = ['python', '-m', 'pip', 'install', '-U' ,'msgpack']
-    msg = ['python', '-m', 'pip', 'install', 'msgpack==1.0.0']
-    nump = ['python', '-m', 'pip', 'install', 'numpy']
-    pyn = ['python', '-m', 'pip', 'install', 'pynvim']
-    pan = ['python', '-m', 'pip', 'install', 'pandas']
+    msgpy = ['python3', '-m', 'pip', 'uninstall', 'msgpack-python']
+    msg = ['python3', '-m', 'pip', 'install', 'msgpack==1.0.0']
+    nump = ['python3', '-m', 'pip', 'install', 'numpy']
+    pyn = ['python3', '-m', 'pip', 'install', 'pynvim']
+    pan = ['python3', '-m', 'pip', 'install', 'pandas']
+
+    msgpy_win = ['python', '-m', 'pip', 'uninstall', 'msgpack-python']
+    msg_win = ['python', '-m', 'pip', 'install', 'msgpack==1.0.0']
+    nump_win = ['python', '-m', 'pip', 'install', 'numpy']
+    pyn_win = ['python', '-m', 'pip', 'install', 'pynvim']
+    pan_win = ['python', '-m', 'pip', 'install', 'pandas']
+
+    def msgpy_win_method(self):
+        try:
+            ret_msgpy_win = subprocess.run(self.msgpy_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_msgpy_win)
+        except Exception:
+            traceback.print_exc()
+    def msg_win_method(self):
+        try:
+            ret_msg_win = subprocess.run(self.msg_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_msg_win)
+        except Exception:
+            traceback.print_exc()
+    def nump_win_method(self):
+        try:
+            ret_nump_win = subprocess.run(self.nump_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_nump_win)
+        except Exception:
+            traceback.print_exc()
+    def pyn_win_method(self):
+        try:
+            ret_pyn_win = subprocess.run(self.pyn_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_pyn_win)
+        except Exception:
+            traceback.print_exc()
+    def pan_win_method(self):
+        try:
+            ret_pan_win = subprocess.run(self.pan_win, encoding='utf-8', stderr=subprocess.PIPE)
+            print(ret_pan_win)
+        except Exception:
+            traceback.print_exc()
 
     def msgpy_method(self):
         try:
@@ -41,11 +77,42 @@ class InstallerClass:
         except Exception:
             traceback.print_exc()
 
-InstClass = InstallerClass()
-InstClass.msgpy_method()
-InstClass.msg_method()
-InstClass.nump_method()
-InstClass.pyn_method()
-InstClass.pan_method()
+
+if sys.version_info[0] == 2:
+  print("This installer is Python2 not supported.")
+
+elif sys.version_info[0] == 3:
+    pf = platform.system()
+    if pf == 'Windows':
+       InstClass = InstallerClass()
+       InstClass.msgpy_win_method()
+       InstClass.msg_win_method()
+       InstClass.nump_win_method()
+       InstClass.pyn_win_method()
+       InstClass.pan_win_method()
+
+    elif pf == 'Darwin':
+       InstClass = InstallerClass()
+       InstClass.msgpy_method()
+       InstClass.msg_method()
+       InstClass.nump_method()
+       InstClass.pyn_method()
+       InstClass.pan_method()
+
+    elif pf == 'Linux':
+       InstClass = InstallerClass()
+       InstClass.msgpy_method()
+       InstClass.msg_method()
+       InstClass.nump_method()
+       InstClass.pyn_method()
+       InstClass.pan_method()
+
+    else:
+       print("Windows and MacOS and Linux other OS not supported.")
+
+
+else:
+  print("Python2 and Python3 Version is No match.")
+
 
 importlib.reload(site)
