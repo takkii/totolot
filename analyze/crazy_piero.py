3b99aa1615bdfa166e0af994fd78f46daa78fc70
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-21 18:41:41 +0900

    Update, use class and method.

--- analyze/crazy_piero.py
+++ analyze/crazy_piero.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 import random, os, sys
 
-saikoro = ["⚀","⚁","⚂","⚃","⚄","⚅"]
+class Dice:
+    def __init__(self):
+      self.saikoro = ["⚀","⚁","⚂","⚃","⚄","⚅"]
+      self.num = random.randint(0,1)
 
-num = random.randint(0,1)
+eyes = Dice().saikoro
+ice = Dice().num
 
-if(num == 0):
-  sin = ' '.join([random.choice(saikoro) for i in range(5)])
+if(ice == 0):
+  sin = ' '.join([random.choice(eyes) for i in range(5)])
   print("Dice shuffle 5 times [心] ... " + sin)
-elif(num == 1): 
-  gi = ' '.join([random.choice(saikoro) for i in range(5)])
+elif(ice == 1): 
+  gi = ' '.join([random.choice(eyes) for i in range(5)])
   print("Dice shuffle 5 times [義] ... " + gi)
 else:
   print('Error? ... not use.')
