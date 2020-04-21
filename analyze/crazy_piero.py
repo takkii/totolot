ec73e3df8df2a8c560debd01f0090968db9fc098
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-04-21 17:53:20 +0900

    Update, saikoro.

--- analyze/crazy_piero.py
+++ analyze/crazy_piero.py
@@ -1,5 +1,16 @@
-import random
+# -*- coding: utf-8 -*-
+import random, os, sys
 
 saikoro = ["⚀","⚁","⚂","⚃","⚄","⚅"]
-han = ' '.join([random.choice(saikoro) for i in range(5)])
-print("Dice shuffle 5 times ... " + han)
+
+num = random.randint(0,1)
+
+if(num == 0):
+  sin = ' '.join([random.choice(saikoro) for i in range(5)])
+  print("Dice shuffle 5 times [心] ... " + sin)
+elif(num == 1): 
+  gi = ' '.join([random.choice(saikoro) for i in range(5)])
+  print("Dice shuffle 5 times [義] ... " + gi)
+else:
+  print('Error? ... not use.')
+
