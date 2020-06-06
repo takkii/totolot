b42da16fc3fc3ab7b3bf0641449bbd3542cca2a6
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-06-06 17:21:31 +0900

    Update, pep8.

--- rplugin/python3/deoplete/sources/totolot.py
+++ rplugin/python3/deoplete/sources/totolot.py
@@ -1,6 +1,9 @@
+import os
+import re
+import sys
+import traceback
 import pandas as pd
 from pandas import Series
-import os, re, sys, traceback
 from deoplete.source.base import Base
 from os.path import expanduser
 
